abef2d51774a0b5b64daaf85e0f6d7594425848d
Leah Loughran <lloughran@hirevue.com>
2014-01-17 10:18:33 -0700

    Started making custom filter for class level

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-var classregApp = angular.module('classregApp', []);
+var classregApp = angular.module('classregApp', ['classregApp.filters']);
 
 classregApp.controller('ClassListCtrl', function($scope, $http) {
     $http.get('classes/classes.json').success(function(data) {
@@ -12,3 +12,27 @@ classregApp.controller('ClassListCtrl', function($scope, $http) {
     })
 
 });
+
+//filterOptions:
+// - dept
+// - searchQuery (search names & descriptions)
+// - classLevel
+//   - classLevel.100, classLevel.200, classLevel.300, classLevel.400, classLevel.500 : (each true/false)
+
+angular.module('classregApp.filters', [])
+    .filter('classFilter', [function () {
+        return function (classes, filterOptions) {
+            if (!angular.isUndefined(classes) && !angular.isUndefined(filterOptions)) {
+                var tempClasses = [];
+
+                classregApp.filter('filter')(classes, filterOptions);
+
+                angular.forEach(classes, function (_class) {
+                    tempClasses.push(_class);
+                });
+                return tempClasses;
+            } else {
+                return classes;
+            }
+        };
+    }]);
