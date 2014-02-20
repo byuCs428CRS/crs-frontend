5c5b3934bc8f2d43b91f2468381e1ba66ae46448
joeldenning <joeldenning@gmail.com>
2014-02-19 17:43:23 -0700

    Adding classes: removed from backend since it will all be handled by the front end. Also, added a dummy register.html page that allows for testing the registration process.

--- app/js/register.js
+++ app/js/register.js
@@ -1,24 +1,18 @@
+var brownieElementIds = ["new_year_term", "curr_id", "new_title_code", "page_sequence", "curr_credit", "section_type", "section_num"]
 
 function addClass() {
-    console.log("here")
-    var request = new XMLHttpRequest();
-    request.onload = function () {
-        if( request.status != 200 )
-            alert("status was "+request.status);
-        document.getElementById("response-div").innerHTML = request.responseText;
-        console.log("received")
-    }
-    var url = "https://gamma.byu.edu/ry/ae/prod/registration/cgi/regOfferings.cgi";
-    var method = "POST";
-    var brownieElements = ["new_year_term", "curr_id", "new_title_code", "page_sequence", "curr_credit", "section_type", "section_num"]
     var brownie = ""
     var prefix = ""
-    for( var el in brownieElements ) {
+    for( var i=0; i<brownieElementIds.length; i++ ) {
+        console.log(brownieElementIds[i])
+        var el = document.getElementById(brownieElementIds[i])
         brownie += prefix + el.id + "=" + el.value
         prefix = "&"
     }
-    var postData = "c=regOfferings&e="+encodeURIComponent("@AddClass")+"&brownie="+encodeURIComponent(brownie);
-    console.log("sent")
+    console.log("brownie = "+brownie)
+    document.getElementById("brownie").value = brownie
+    document.getElementById("invisible").submit()
+    console.log("submitted")
 }
 
 
